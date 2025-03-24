5169b6988625125f605257eafe808bf578832e3d
zjpzjp <zjpzjp@foxmail.com>
2025-03-24 14:46:00 +0800

    删除无用依赖，更新所有依赖版本

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import dynamicProxy from './vite.dynamic.proxy'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
   plugins: [
     vue(),
@@ -10,12 +12,24 @@ export default defineConfig({
     dynamicProxy({
       path: new RegExp('^/api'),
       default: 'https://mock.apifox.cn/m1/3332315-0-default/'
+    }),
+    Components({
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass'
+        })
+      ]
     })
   ],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "@/style/var.scss" as *;`
+        additionalData: `@use "@/style/var.scss" as *;`,
+        api: 'modern-compiler'
       }
     }
   },
@@ -24,9 +38,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/element-plus-admin-mini',
+  base: '/',
   server: {
     host: '0.0.0.0',
-    port: 9988
+    port: 8999
   }
 })
